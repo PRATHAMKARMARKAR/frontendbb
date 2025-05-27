019bf038bc8f789439caec49b0c0fdc0fefc20a8
PRATHAMKARMARKAR <karmarkarpratham56@gmail.com>
2025-05-27 17:27:50 +0530

    2 commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,6 @@
 
-
-import React from "react";
-import { useState } from "react";
 import Body from "./components/Body";
-import LandingPage from "./components/landingPage/LandingPage";
-import DashboardDetails from "./components/DashboardDetails/DashboardDetails";
-import Register from "./components/Register/Register";
-import Partneroverview from "./components/Partner/Partneroverview";
-import Profile from "./components/Profile/Profile";
- 
-import Reservation from "./components/reservation/Reservation";
-import Reviews from "./components/Reviews/Reviews";
- 
-import Bookingpage from "./components/Bookingpage/Bookingpage";
-import Onboardingpage from "./components/OnBoarding/Onboardingpage";
-import Storage from "./components/Bookingpage/Storage";
+
  
 function App() {
   return (
